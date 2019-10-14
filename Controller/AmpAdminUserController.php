b28a283f7a3f88fc9bad81d6417f46118b741e8f
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-14 18:27:15 +0530

    changes in api

--- Controller/AmpAdminUserController.php
+++ Controller/AmpAdminUserController.php
@@ -253,4 +253,51 @@ class AmpAdminUserController extends ApiController
             $this->apiResponse['message'] = "your session has been expired";
         }
     }
+
+     /**
+     *  Get Location API
+     */
+    public function getlocation()
+    {
+        header("Access-Control-Allow-Origin: *");
+        $options = array();
+        $options['fields'] = array('id', 'location' => 'DISTINCT location');
+
+        $TblLocation = TableRegistry::get('Location',['table' => 'amp_locations']);
+        $location = $TblLocation->find('all',$options)->toArray();
+        $this->httpStatusCode = 200;
+        $this->apiResponse['locations'] = $location;
+    }
+}
+
+function crypto_rand_secure($min, $max)
+{
+    $range = $max - $min;
+    if ($range < 1) {
+        return $min;
+    }
+    // not so random...
+    $log = ceil(log($range, 2));
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+        $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd > $range);
+    return $min + $rnd;
+}
+
+function getToken($length)
+{
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet .= "0123456789";
+    $max = strlen($codeAlphabet); // edited
+
+    for ($i = 0; $i < $length; $i++) {
+        $token .= $codeAlphabet[crypto_rand_secure(0, $max - 1)];
+    }
+    return $token;
 }
