ec21a5e45dff8d562115ffef6daabdc78a27ac24
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-15 16:32:51 +0530

    changes

--- Controller/AmpEmployeesListingController.php
+++ Controller/AmpEmployeesListingController.php
@@ -1,22 +1,17 @@
 <?php
 namespace App\Controller;
 
-use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+use Cake\Filesystem\File;
+use Cake\Filesystem\Folder;
+use Cake\I18n\Time;
+use Cake\Mailer\Email;
+use RestApi\Controller\ApiController;
 
-/**
- * AmpEmployeesListing Controller
- *
- * @property \App\Model\Table\AmpEmployeesListingTable $AmpEmployeesListing
- *
- * @method \App\Model\Entity\AmpEmployeesListing[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
-class AmpEmployeesListingController extends AppController
+
+class AmpEmployeesListingController extends ApiController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
+    
     public function index()
     {
         $this->paginate = [
@@ -27,85 +22,39 @@ class AmpEmployeesListingController extends AppController
         $this->set(compact('ampEmployeesListing'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Amp Employees Listing id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
+    /**List employee List */
+    public function view()
     {
-        $ampEmployeesListing = $this->AmpEmployeesListing->get($id, [
-            'contain' => ['Emails']
-        ]);
-
-        $this->set('ampEmployeesListing', $ampEmployeesListing);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $ampEmployeesListing = $this->AmpEmployeesListing->newEntity();
-        if ($this->request->is('post')) {
-            $ampEmployeesListing = $this->AmpEmployeesListing->patchEntity($ampEmployeesListing, $this->request->getData());
-            if ($this->AmpEmployeesListing->save($ampEmployeesListing)) {
-                $this->Flash->success(__('The amp employees listing has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The amp employees listing could not be saved. Please, try again.'));
+        if ($this->checkToken()) {
+            header("Access-Control-Allow-Origin: *");
+            $ampEmployeesListing = $this->AmpEmployeesListing->find('all')->toList;
+            $this->httpStatusCode = 200;
+            $this->apiResponse['employeeinfo'] = $ampEmployeesListing;
+        } else{
+            $this->httpStatusCode = 403;
+            $this->apiResponse['message'] = "your session has been expired";
         }
-        $emails = $this->AmpEmployeesListing->Emails->find('list', ['limit' => 200]);
-        $this->set(compact('ampEmployeesListing', 'emails'));
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Amp Employees Listing id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $ampEmployeesListing = $this->AmpEmployeesListing->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $ampEmployeesListing = $this->AmpEmployeesListing->patchEntity($ampEmployeesListing, $this->request->getData());
-            if ($this->AmpEmployeesListing->save($ampEmployeesListing)) {
-                $this->Flash->success(__('The amp employees listing has been saved.'));
+    // /**
+    //  * Add method
+    //  *
+    //  * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+    //  */
+    // public function add()
+    // {
+    //     $ampEmployeesListing = $this->AmpEmployeesListing->newEntity();
+    //     if ($this->request->is('post')) {
+    //         $ampEmployeesListing = $this->AmpEmployeesListing->patchEntity($ampEmployeesListing, $this->request->getData());
+    //         if ($this->AmpEmployeesListing->save($ampEmployeesListing)) {
+    //             $this->Flash->success(__('The amp employees listing has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The amp employees listing could not be saved. Please, try again.'));
-        }
-        $emails = $this->AmpEmployeesListing->Emails->find('list', ['limit' => 200]);
-        $this->set(compact('ampEmployeesListing', 'emails'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Amp Employees Listing id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $ampEmployeesListing = $this->AmpEmployeesListing->get($id);
-        if ($this->AmpEmployeesListing->delete($ampEmployeesListing)) {
-            $this->Flash->success(__('The amp employees listing has been deleted.'));
-        } else {
-            $this->Flash->error(__('The amp employees listing could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
+    //             return $this->redirect(['action' => 'index']);
+    //         }
+    //         $this->Flash->error(__('The amp employees listing could not be saved. Please, try again.'));
+    //     }
+    //     $emails = $this->AmpEmployeesListing->Emails->find('list', ['limit' => 200]);
+    //     $this->set(compact('ampEmployeesListing', 'emails'));
+    // }
+   
 }
