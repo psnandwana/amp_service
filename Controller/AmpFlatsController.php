0fad5424aa99b4b65257e8e7ef4ccbe16343cb57
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-16 11:11:59 +0530

    changes

--- Controller/AmpFlatsController.php
+++ Controller/AmpFlatsController.php
@@ -47,33 +47,32 @@ class AmpFlatsController extends ApiController
         $flat_band = $data['flat_band'];
 
         $queryInsert = $this->AmpFlats->query();
-        // $queryInsert->insert(['flat_no', 'apartment_name', 'flat_type', 'agreement_status', 'agreement_date', 'address', 'pincode', 'city', 'state', 'longitude', 'latitude', 'rent_amount', 'maintenance_amount', 'owner_name', 'owner_mobile_no', 'owner_email', 'vacancy_status', 'flat_capacity', 'flat_band', 'created_date'])
-        $queryInsert->insert(['flat_no', 'apartment_name','agreement_status', 'agreement_date','created_date'])
+        $queryInsert->insert(['flat_no', 'apartment_name', 'flat_type', 'agreement_status', 'agreement_date', 'address', 'pincode', 'city', 'state', 'longitude', 'latitude', 'rent_amount', 'maintenance_amount', 'owner_name', 'owner_mobile_no', 'owner_email', 'vacancy_status', 'flat_capacity', 'flat_band', 'created_date'])
             ->values([
                 'flat_no' => $flat_no,
                 'apartment_name' => $apartment_name,
                 'flat_type' => $flat_type,
-                // 'agreement_status' => $agreement_status,
-                // 'agreement_date' => $agreement_date,
-                // 'address' => $address,
-                // 'pincode' => $pincode,
-                // 'city' => $city,
-                // 'state' => $state,
-                // 'longitude' => $longitude,
-                // 'latitude' => $latitude,
-                // 'rent_amount' => $rent_amount,
-                // 'maintenance_amount' => $maintenance_amount,
-                // 'owner_name' => $owner_name,
-                // 'owner_mobile_no' => $owner_mobile_no,
-                // 'owner_email' => $owner_email,
-                // 'vacancy_status' => $vacancy_status,
-                // 'flat_capacity' => $flat_capacity,
-                // 'flat_band' => $flat_band,
+                'agreement_status' => $agreement_status,
+                'agreement_date' => $agreement_date,
+                'address' => $address,
+                'pincode' => $pincode,
+                'city' => $city,
+                'state' => $state,
+                'longitude' => $longitude,
+                'latitude' => $latitude,
+                'rent_amount' => $rent_amount,
+                'maintenance_amount' => $maintenance_amount,
+                'owner_name' => $owner_name,
+                'owner_mobile_no' => $owner_mobile_no,
+                'owner_email' => $owner_email,
+                'vacancy_status' => $vacancy_status,
+                'flat_capacity' => $flat_capacity,
+                'flat_band' => $flat_band,
                 'created_date' => Time::now(),
             ])
             ->execute();
         $this->httpStatusCode = 200;
-        $this->apiResponse['message'] = 'flat details has been updated successfully.';
+        $this->apiResponse['message'] = 'flat profile has been  successfully.';
         // } else {
         //     $this->httpStatusCode = 403;
         //     $this->apiResponse['message'] = "your session has been expired";
