7278c70b63613ff8cf5f021a04115683359f90ae
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-17 14:27:06 +0530

    added session check

--- Controller/AmpEmployeesListingController.php
+++ Controller/AmpEmployeesListingController.php
@@ -1,21 +1,15 @@
 <?php
 namespace App\Controller;
 
-use Cake\Datasource\ConnectionManager;
-use Cake\Filesystem\File;
-use Cake\Filesystem\Folder;
-use Cake\I18n\Time;
-use Cake\Mailer\Email;
 use RestApi\Controller\ApiController;
-
 
 class AmpEmployeesListingController extends ApiController
 {
     /**List employee List */
     public function index()
     {
+        header("Access-Control-Allow-Origin: *");
         if ($this->checkToken()) {
-            header("Access-Control-Allow-Origin: *");
             $page = $this->request->getData('page');
             $this->paginate = ['limit' => 10, 'page' => $page];
             $ampEmployeesListing = $this->paginate($this->AmpEmployeesListing);
@@ -36,16 +30,22 @@ class AmpEmployeesListingController extends ApiController
     public function filteremployee()
     {
         header("Access-Control-Allow-Origin: *");
-        $options = array();
-        $emp_code = $this->request->getData('emp_code');
-        $options['conditions'] = array('emp_code LIKE'=> $emp_code.'%');
-        $employees = $this->AmpEmployeesListing->find('all',$options)->toArray();
-        $tmp_array = array();
-        foreach ($employees as $value) {
-            $tmp_array[] = $value;
+        if ($this->checkToken()) {
+            $options = array();
+            $emp_code = $this->request->getData('emp_code');
+            $options['conditions'] = array('emp_code LIKE' => $emp_code . '%');
+            $employees = $this->AmpEmployeesListing->find('all', $options)->toArray();
+            $tmp_array = array();
+            foreach ($employees as $value) {
+                $tmp_array[] = $value;
+            }
+            $this->httpStatusCode = 200;
+            $this->apiResponse['employees'] = $tmp_array;
+        } else {
+            $this->httpStatusCode = 403;
+            $this->apiResponse['message'] = "your session has been expired";
         }
-        $this->httpStatusCode = 200;
-        $this->apiResponse['employees'] = $tmp_array;
+
     }
-    
+
 }
