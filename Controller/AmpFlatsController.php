984866f7231f7c77cc3f822263c2d08729ca0e2e
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-18 19:11:56 +0530

    changes in assign flat

--- Controller/AmpFlatsController.php
+++ Controller/AmpFlatsController.php
@@ -431,7 +431,7 @@ class AmpFlatsController extends ApiController
             $roomID = $this->request->data('room_id');
             $roomCapapcity = $flatRoomMappingTable->find('all')->where(['id' => $roomID])->toList();
             $employeeCount = $roomEmployeeMappingTable->find('all')->where(['room_id' => $roomID])->toList();
-            dd($roomCapapcity);
+            dd($employeeCount);
             if (count($room) > 0) {
                 $capacity = $roomCapapcity[0]['capacity'];
             }
