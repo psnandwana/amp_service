0a96feaa59bc6aafd780cfab5a25f20eb0ae3a92
jadhavkarthik <jadhavkarthik24@gmail.com>
2019-10-22 16:23:40 +0530

    debugging userflatdetails

--- Controller/AmpEmployeesListingController.php
+++ Controller/AmpEmployeesListingController.php
@@ -53,10 +53,11 @@ class AmpEmployeesListingController extends ApiController
         header("Access-Control-Allow-Origin: *");
         if ($this->checkToken()) {
             $emp_id = $this->request->getData('emp_id');
-            dd($emp_id);
+            // dd($emp_id);
+            $emp_id = (int)$emp_id;
             $roomEmployeeMappingTable = TableRegistry::get('RoomEmpMap', ['table' => 'amp_room_employee_mapping']);
             $empExists = $roomEmployeeMappingTable->find('all')->where(['RoomEmpMap.employee_id' => $emp_id, 'RoomEmpMap.active_status' => '1'])->count();
-            echo $empExists;exit;
+            dd($empExists);
             if ($empExists > 0) {
                 $options = array();
                 $options['conditions']['RoomEmpMap.employee_id'] = $emp_id;
