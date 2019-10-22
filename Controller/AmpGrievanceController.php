802fd035a0a968a515d63ab7cb342bdcb000311a
psnandwana <pradip.nandwana@indianpac.com>
2019-10-22 15:29:45 +0530

    create request

--- Controller/AmpGrievanceController.php
+++ Controller/AmpGrievanceController.php
@@ -19,6 +19,30 @@ class AmpGrievanceController extends ApiController
         $this->set(compact('ampGrievance'));
     }
 
+    public function create()
+    {
+        header("Access-Control-Allow-Origin: *");
+        if ($this->checkToken()) {
+            date_default_timezone_set('Asia/Kolkata');
+            $current_date = date('Y-m-d H:i:s');
+            $AmpGrievance = $this->AmpGrievance->newEntity();
+           
+            $this->request->data['submitted_date'] = $current_date;
+            $this->request->data['status'] = 'Pending';
+            $AmpGrievance = $this->AmpGrievance->patchEntity($AmpGrievance, $this->request->getData());
+            if ($this->AmpGrievance->save($AmpGrievance)) {
+                $this->httpStatusCode = 200;
+                $this->apiResponse['message'] = 'Your request has been submitted successfully';
+            } else {
+                $this->httpStatusCode = 422;
+                $this->apiResponse['message'] = 'Unable to submit your request';
+            }
+        } else {
+            $this->httpStatusCode = 403;
+            $this->apiResponse['message'] = "your session has been expired";
+        }
+    }
+
     public function getrequesttype()
     {
         header("Access-Control-Allow-Origin: *");
